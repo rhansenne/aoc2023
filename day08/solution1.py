3661965e9fd03de255a0c3f5dcab87da04c8d047
rhansenne <rami.hansenne@digipolis.gent>
2023-12-08 12:46:48 +0100

    day08

--- day08/solution1.py
+++ day08/solution1.py
@@ -4,6 +4,6 @@ network=dict([(l[0:3],(l[7:10],l[12:15])) for l in doc[2:]])
 node='AAA'
 steps=0
 while node != 'ZZZ':
-    node = network[node][0] if instr[steps%len(instr)]=='L' else network[node][1]
+    node = network[node][0 if instr[steps%len(instr)]=='L' else 1]
     steps+=1
 print(steps)
--- day08/solution2.py
+++ day08/solution2.py
@@ -6,7 +6,7 @@ steps=set()
 for node in filter(lambda n: n[2] == 'A', network.keys()): 
     step=0
     while node[2] != 'Z':
-        node = network[node][0] if instr[step%len(instr)]=='L' else network[node][1]
+        node = network[node][0 if instr[step%len(instr)]=='L' else 1]
         step+=1
     steps.add(step)
 print(math.lcm(*steps))
