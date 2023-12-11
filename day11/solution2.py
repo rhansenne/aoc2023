58e62c6ef9dfca2e602da577beb5d1bb8f870675
Hansenne Rami <Rami.Hansenne@district09.gent>
2023-12-11 17:01:42 +0100

    day11

---
+++ day11/solution2.py
@@ -0,0 +1,18 @@
+import copy, itertools as it, numpy as np
+image = np.array([[*l.strip()] for l in open('input.txt', 'r').readlines()])
+coords=[]
+exp=0
+for i,row in enumerate(image):
+    if np.all(row=='.'):
+        exp+=999999
+    for j,c in enumerate(row):
+        if c=='#':
+            coords.append([i+exp,j])
+coords_exp=copy.deepcopy(coords)
+for j,col in enumerate(image.T):
+    if np.all(col=='.'):   
+        for c,co in enumerate(coords):
+            if co[1]>j:
+                coords_exp[c][1]+=999999
+total=sum([abs(g1[0]-g2[0])+abs(g1[1]-g2[1]) for g1,g2 in it.combinations(coords_exp,2)])
+print(total)
